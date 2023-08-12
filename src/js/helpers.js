908566bbd997f08a74e4e85271debb44b900d65f
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2023-08-12 01:13:57 -0400

    html without styles and js logic complete

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,1 +1,28 @@
-console.log("from helpers.js");
+import { fetchCountries } from "./api";
+
+export function parseTime(date) {
+  if (!(date instanceof Date && !isNaN(date.getTime()))) {
+    return "Invalid Date";
+  }
+  const options = {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  };
+  return new Intl.DateTimeFormat("en-US", options).format(date);
+}
+
+export function setImageSrc(element) {
+  let theme = localStorage.getItem("theme");
+
+  if (theme === "dracula") {
+    return element.setAttribute("src", "/image-for-dark-mode");
+  } else {
+    return element.setAttribute("src", "/image-for-light-mode");
+  }
+}
+
+export let countries = [];
+export async function loadCountries() {
+  countries = await fetchCountries();
+}
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,63 @@
-console.log("from const.js");
+import ArrivalFlightsList from "../components/ArrivalsFlightsList";
+import DepartureFlightsList from "../components/DepartureFlightsList";
+import { fetchAirports } from "./api";
+import { countries, loadCountries, setImageSrc } from "./helpers";
+import {
+  airportInput,
+  clearButton,
+  countryInput,
+  heroImage,
+  invalidText,
+  mainForm,
+  navLogo,
+  themeSelector,
+} from "./const";
 
-const navbar = document.querySelector("#navbar");
-const content = document.querySelector("#content");
-const footer = document.querySelector("#footer");
+// Set images src based on theme saved to local storage.
+setImageSrc(navLogo);
+setImageSrc(heroImage);
+loadCountries();
+export let airports = [];
 
-navbar.addEventListener("click", (e) =>
-  console.log("navbar clicked", e.target)
-);
-content.addEventListener("click", (e) =>
-  console.log("navbar clicked", e.target)
-);
-footer.addEventListener("click", (e) =>
-  console.log("navbar clicked", e.target)
-);
+countryInput.addEventListener("input", validateCountry);
+// themeSelector.addEventListener("click", handleThemeSelector);
+clearButton.addEventListener("click", handleClearButton);
+countryInput.addEventListener("blur", validateCountry);
+mainForm.addEventListener("submit", handleFormSubmit);
+
+function handleThemeSelector() {
+  setTimeout(() => {
+    setImageSrc(navLogo);
+    setImageSrc(heroImage);
+  }, 20);
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const countryCode = countryInput.dataset.value;
+  const depFlightsList = new DepartureFlightsList(countryCode);
+  const arrFlightsList = new ArrivalFlightsList(countryCode);
+  depFlightsList.createList(airportInput.dataset.value);
+  arrFlightsList.createList(airportInput.dataset.value);
+}
+
+function handleClearButton() {
+  location.reload();
+}
+
+async function validateCountry() {
+  const inputVal = countryInput.value.trim();
+  const countryCode = countryInput.dataset.value;
+  const isValidCountry = countries.some(
+    (country) => country.label.toLowerCase() === inputVal.toLowerCase()
+  );
+
+  if (!isValidCountry && inputVal !== "") {
+    setTimeout(() => {
+      invalidText.innerText = "Invalid country.";
+    }, 100);
+  } else {
+    invalidText.innerText = "";
+    airports = await fetchAirports(countryCode);
+  }
+}
